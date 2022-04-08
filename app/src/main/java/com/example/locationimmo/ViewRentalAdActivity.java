2e41b39e0212afb4f10d255e4c81ff573ce82fba
Gréviste <alexandre.spatola@outlook.com>
2022-04-08 17:48:01 +0200

    Added missing activities : chat and ad view

---
+++ app/src/main/java/com/example/locationimmo/ViewRentalAdActivity.java
@@ -0,0 +1,14 @@
+package com.example.locationimmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRentalAdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_rental_ad);
+    }
+}
