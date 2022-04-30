4889d36d77fb3b3fb9cd67b0fb7a2ed63ebbf383
Lanvin Alexandre <alexandre.lanvin@etu.umontpellier.fr>
2022-04-30 19:21:02 +0200

    Authentication + signUP + database_v1 added

--- app/src/main/java/com/example/locationimmo/MainActivity.java
+++ app/src/main/java/com/example/locationimmo/MainActivity.java
@@ -5,13 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    JSONObject db = new JSONObject();
+    EditText mail;
+    EditText pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mail = findViewById(R.id.editTextTextEmailAddress);
+        pwd = findViewById(R.id.editTextTextPassword);
     }
 
     public void loadSearch(View view) {
@@ -24,5 +41,67 @@ public class MainActivity extends AppCompatActivity {
     public void loadSignUp(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    public boolean checkAuth(EditText mail, EditText pwd) {
+        String mail_str = mail.getText().toString();
+        String pwd_str = pwd.getText().toString();
+        boolean grant_access = false;
+
+        System.out.println(db.names());
+
+        try {
+            JSONArray usr_arr = db.getJSONArray("user");
+            for(int i = 1; i < usr_arr.length(); i ++){
+                JSONObject usr = usr_arr.getJSONObject(i);
+
+                if(usr.get("e-mail").equals(mail_str) && usr.get("pwd").equals(pwd_str))
+                    grant_access = true;
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return grant_access;
+    }
+
+    public void signIn(View view) {
+        FileInputStream in_stream = null;
+
+        //Read DB
+        try {
+            in_stream = getApplicationContext().openFileInput("database.json");
+            String file_content = "";
+            int current_c;
+            while((current_c = in_stream.read()) != -1){
+                file_content += (char)current_c;
+            }
+            in_stream.close();
+            db = new JSONObject(file_content);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DB LOADED: " + db.toString());
+        //authentification
+        boolean granted = checkAuth(mail, pwd);
+
+        if(granted){
+            System.out.println("ACCESS GRANTED");
+            Intent intent = new Intent(this, EditRentalAdActivity.class);
+            startActivity(intent);
+        }else{
+            Toast tst = new Toast(this);
+            tst.setText("CREATE AN ACCOUNT FIRST.");
+            tst.show();
+        }
+
     }
 }
--- app/src/main/java/com/example/locationimmo/SignUpActivity.java
+++ app/src/main/java/com/example/locationimmo/SignUpActivity.java
@@ -2,13 +2,128 @@ package com.example.locationimmo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
+    JSONObject db = new JSONObject();
+
+    EditText mail;
+    EditText pwd_1;
+    EditText pwd_2;
+    RadioButton[] type;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mail = findViewById(R.id.editTextTextEmailAddressSU);
+        pwd_1 = findViewById(R.id.editTextTextPasswordSU);
+        pwd_2 = findViewById(R.id.editTextTextPasswordConfirm);
+        type = new RadioButton[]{
+                findViewById(R.id.radioButtonTenant),
+                findViewById(R.id.radioButtonIndividual),
+                findViewById(R.id.radioButtonProfessional),
+        };
+
+        try {
+            db.put("user","");
+            db.put("ad","");;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> getFields() {
+        HashMap<String, String> fields = new HashMap<String, String>();
+        fields.put("e-mail", mail.getText().toString());
+        fields.put("pwd", pwd_1.getText().toString());
+        for(RadioButton radio : type) {
+            if(radio.isChecked()){
+                fields.put("type", radio.getText().toString());
+            }
+        }
+
+        for(Map.Entry<String, String> entry : fields.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        return fields;
+    }
+
+    public void resetDB(View view){
+        try {
+            System.out.println("Resetting db ...");
+            FileOutputStream out_stream = getApplicationContext().openFileOutput("database.json", Context.MODE_PRIVATE);
+            out_stream.write("".getBytes());
+            out_stream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateJson(View view) {
+        HashMap<String, String> data = getFields();
+
+        FileInputStream in_stream = null;
+
+        //Read DB
+        try {
+            in_stream = getApplicationContext().openFileInput("database.json");
+            String file_content = "";
+            int current_c;
+            while((current_c = in_stream.read()) != -1){
+                file_content += (char)current_c;
+            }
+            in_stream.close();
+            JSONObject json_fc = new JSONObject(file_content);
+            db.put("user", json_fc.get("user"));
+            System.out.println("DB state before update: " + db.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Update DB
+        try {
+
+            db.accumulate("user", new JSONObject(data));
+            System.out.println("DB after update? " + db.toString());
+            FileOutputStream out_stream = getApplicationContext().openFileOutput("database.json", Context.MODE_PRIVATE);
+            out_stream.write(db.toString().getBytes());
+            out_stream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
