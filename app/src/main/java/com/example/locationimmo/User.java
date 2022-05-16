f112e46ebef567139f93e53776a784bb2dc57be4
Gréviste <alexandre.spatola@outlook.com>
2022-05-16 14:54:48 +0200

    Replace ads array with ArrayList

--- app/src/main/java/com/example/locationimmo/User.java
+++ app/src/main/java/com/example/locationimmo/User.java
@@ -1,10 +1,11 @@
 package com.example.locationimmo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class User implements Serializable {
     public String email;
     public String password;
     public UserType type;
-    public RentalAd[] ads;
+    public ArrayList<RentalAd> ads;
 }
